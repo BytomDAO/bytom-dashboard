00df40e2c90705904eb4a7e5e5ace932a7e909dc
JNet <jnet@MacBook-Pro.localdomain>
2018-01-29 11:02:44 +0800

    transaction 分页

--- src/features/transactions/components/List.jsx
+++ src/features/transactions/components/List.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { BaseList } from 'features/shared/components'
+import { BaseList, Pagination } from 'features/shared/components'
 import ListItem from './ListItem/ListItem'
 import actions from 'actions'
+import { pageSize } from '../../../utility/environment'
 
 const type = 'transaction'
+
 
 class List extends React.Component {
   componentWillReceiveProps(nextProps) {
@@ -13,16 +15,27 @@ class List extends React.Component {
       }
     }
   }
-
   render() {
     const ItemList = BaseList.ItemList
-    return (<ItemList {...this.props} />)
+    //pagination create
+    let pagination = null
+    if(!this.props.noResults){
+      pagination = <Pagination
+        currentPage={this.props.currentPage}
+        isLastPage={this.props.isLastPage}
+        pushList={this.props.pushList}/>
+    }
+
+    return <div>
+      <ItemList {...this.props}/>
+      {pagination}
+    </div>
   }
 }
 
 export default BaseList.connect(
   (state, ownProps) => ({
-    ...BaseList.mapStateToProps(type, ListItem)(state, ownProps),
+    ...mapStateToProps(type,ListItem)(state,ownProps),
     blockHeight: state.core.blockHeight
   }),
   (dispatch) => ({
@@ -31,3 +44,30 @@ export default BaseList.connect(
   }),
   List
 )
+
+const mapStateToProps = (type, itemComponent, additionalProps = {}) => {
+  return (state, ownProps) => {
+    const currentPage = Math.max(parseInt(ownProps.location.query.page) || 1, 1)
+    const lastPageIndex = pageSize - 1
+    const isLastPage = ((currentPage - 1) == lastPageIndex)
+    const startIndex = (currentPage - 1) * pageSize
+    const totalItems = state[type].items
+    const keysArray = Object.keys(totalItems)
+    const currentItems = keysArray.slice(startIndex, startIndex + pageSize).map(
+      id => totalItems[id]
+    ).filter(item => item != undefined)
+
+    return {
+      currentPage: currentPage,
+      isLastPage: isLastPage,
+      items: currentItems,
+      loadedOnce: state[type].queries.loadedOnce,
+      type: type,
+      listItemComponent: itemComponent,
+      noResults: currentItems.length == 0,
+      showFirstTimeFlow: currentItems.length == 0,
+      ...additionalProps
+    }
+  }
+}
+
