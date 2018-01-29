08c288000b927f1c67afe8259bcb1a6f9fee53a8
Zhiting Lin <zlin035@uottawa.ca>
2018-01-29 18:59:51 +0800

    Hide the next Pagination button in the last page

--- src/utility/environment.js
+++ src/utility/environment.js
@@ -28,6 +28,7 @@ export const history = useRouterHistory(createHistory)({
 })
 
 export const pageSize = 25
+export const UTXOpageSize = 4
 
 export const testnetInfoUrl = process.env.TESTNET_INFO_URL || 'https://testnet-info.chain.com'
 export const testnetUrl = process.env.TESTNET_GENERATOR_URL || 'https://testnet.chain.com'
