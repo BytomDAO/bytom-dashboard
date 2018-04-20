2ff39bc3a1153b446244ab05430e58baec90c6ca
Yongfeng LI <wliyongfeng@gmail.com>
2018-04-20 16:58:48 +0800

    add restore wallet to account init page

--- src/features/app/components/Container.jsx
+++ src/features/app/components/Container.jsx
@@ -67,7 +67,7 @@ class Container extends React.Component {
     if (!this.props.authOk) {
       layout = <Login/>
     } else if (!this.props.configKnown) {
-      return <Loading>Connecting to Chain Core...</Loading>
+      return <Loading>Connecting to Bytom Core...</Loading>
     } else if (!this.props.configured) {
       layout = <Config>{this.props.children}</Config>
     } else if (!this.props.accountInit && this.state.noAccountItem){
--- src/features/app/components/Register/Register.jsx
+++ src/features/app/components/Register/Register.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { ErrorBanner, TextField } from 'features/shared/components'
+import {connect} from 'react-redux'
+import {ErrorBanner, TextField} from 'features/shared/components'
 import actions from 'actions'
 import styles from './Register.scss'
-import { reduxForm } from 'redux-form'
+import {reduxForm} from 'redux-form'
+import {chainClient} from 'utility/environment'
+import {push} from 'react-router-redux'
 
 class Register extends React.Component {
   constructor(props) {
     super(props)
+    this.connection = chainClient().connection
 
     this.submitWithErrors = this.submitWithErrors.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      init: true
+    })
   }
 
   submitWithErrors(data) {
@@ -19,11 +28,46 @@ class Register extends React.Component {
     })
   }
 
+  setMode(isInit) {
+    this.setState({
+      init: isInit
+    })
+  }
+
+  restore() {
+    const element = document.getElementById('bytom-restore-file-upload-init')
+    element.click()
+  }
+
+  handleFileChange(event) {
+    const files = event.target.files
+    if (files.length <= 0) {
+      this.setState({key: null})
+      return
+    }
+
+    const fileReader = new FileReader()
+    fileReader.onload = fileLoadedEvent => {
+      const backupData = JSON.parse(fileLoadedEvent.target.result)
+      this.connection.request('/restore-wallet', backupData).then(resp => {
+        if (resp.status === 'fail') {
+          this.props.showError(new Error(resp.msg))
+          return
+        }
+        this.props.success()
+      })
+    }
+    fileReader.readAsText(files[0], 'UTF-8')
+
+    const fileElement = document.getElementById('bytom-restore-file-upload-init')
+    fileElement.value = ''
+  }
+
   render() {
-    const lang =this.props.lang
+    const lang = this.props.lang
 
     const {
-      fields: { keyAlias, password, repeatPassword, accountAlias },
+      fields: {keyAlias, password, repeatPassword, accountAlias},
       error,
       handleSubmit,
       submitting
@@ -31,39 +75,77 @@ class Register extends React.Component {
 
     return (
       <div className={styles.main}>
-        <h2 className={styles.title}>{lang==='zh'? '初始账户和密钥':'Init your account and key'}</h2>
-        <div className={styles.form}>
-          <form onSubmit={handleSubmit(this.submitWithErrors)}>
-            <TextField
-              title={lang==='zh'? '账户别名':'Account Alias'}
-              placeholder={lang==='zh'? '请输入账户别名...':'Please enter the account alias...'}
-              fieldProps={accountAlias}
-              autoFocus={true} />
-            <TextField
-              title={lang==='zh'? '密钥别名':'Key Alias'}
-              placeholder={lang==='zh'? '请输入密钥别名...':'Please enter the key alias...'}
-              fieldProps={keyAlias} />
-            <TextField
-              title={lang==='zh'? '密钥密码':'Key Password'}
-              placeholder={lang==='zh'? '请输入密钥密码...':'Please enter the key password...'}
-              fieldProps={password}
-              type='password' />
-            <TextField
-              title={lang==='zh'? '重复输入密钥密码':'Repeat your key password'}
-              placeholder={lang==='zh'? '请重复输入密钥密码...':'Please repeat the key password...'}
-              fieldProps={repeatPassword}
-              type='password' />
-
-            {error &&
-              <ErrorBanner
-                title='Error in Init Account..'
-                error={error} />}
-
-            <button type='submit' className='btn btn-primary' disabled={submitting}>
-              {lang==='zh'? '注册':'Register'}
-            </button>
-          </form>
+        <div>
+          <a className={`${styles.choice} ${this.state && this.state.init ? styles.active : ''}`}
+             href='javascript:;' onClick={this.setMode.bind(this, true)}>
+            {lang === 'zh' ? '初始账户和密钥' : 'Init your account and key'}
+          </a>
+          <a className={`${styles.choice} ${styles.restore} ${this.state && this.state.init ? '' : styles.active}`}
+             href='javascript:;' onClick={this.setMode.bind(this, false)}>
+            {lang === 'zh' ? '恢复钱包' : 'Restore wallet'}
+          </a>
         </div>
+
+        {
+          this.state.init &&
+          <div>
+            <h2 className={styles.title}>{lang === 'zh' ? '初始账户和密钥' : 'Init your account and key'}</h2>
+            <div className={styles.form}>
+              <form onSubmit={handleSubmit(this.submitWithErrors)}>
+                <TextField
+                  title={lang === 'zh' ? '账户别名' : 'Account Alias'}
+                  placeholder={lang === 'zh' ? '请输入账户别名...' : 'Please enter the account alias...'}
+                  fieldProps={accountAlias}
+                  autoFocus={true}/>
+                <TextField
+                  title={lang === 'zh' ? '密钥别名' : 'Key Alias'}
+                  placeholder={lang === 'zh' ? '请输入密钥别名...' : 'Please enter the key alias...'}
+                  fieldProps={keyAlias}/>
+                <TextField
+                  title={lang === 'zh' ? '密钥密码' : 'Key Password'}
+                  placeholder={lang === 'zh' ? '请输入密钥密码...' : 'Please enter the key password...'}
+                  fieldProps={password}
+                  type='password'/>
+                <TextField
+                  title={lang === 'zh' ? '重复输入密钥密码' : 'Repeat your key password'}
+                  placeholder={lang === 'zh' ? '请重复输入密钥密码...' : 'Please repeat the key password...'}
+                  fieldProps={repeatPassword}
+                  type='password'/>
+
+                {error &&
+                <ErrorBanner
+                  title='Error in Init Account..'
+                  error={error}/>}
+
+                <button type='submit' className='btn btn-primary' disabled={submitting}>
+                  {lang === 'zh' ? '注册' : 'Register'}
+                </button>
+              </form>
+            </div>
+          </div>
+        }
+        {
+          !this.state.init &&
+          <div>
+            <h2 className={styles.title}>{lang === 'zh' ? '恢复钱包' : 'Restore wallet'}</h2>
+            <div className={styles.form}>
+              <form onSubmit={handleSubmit(this.submitWithErrors)}>
+                <button className='btn btn-primary' onClick={this.restore.bind(this)}>
+                  {lang === 'zh' ? '恢复' : 'Restore'}
+                </button>
+                <p></p>
+                {
+                  lang === 'zh' && <p>点击恢复按钮，选择备份文件进行恢复</p>
+                }
+                {
+                  lang != 'zh' && <p>Click the button, and select the backup file</p>
+                }
+                <input id='bytom-restore-file-upload-init' type='file' style={{'display': 'none'}}
+                       onChange={this.handleFileChange.bind(this)}/>
+              </form>
+            </div>
+          </div>
+        }
       </div>
     )
   }
@@ -77,10 +159,10 @@ const validate = values => {
   }
   if (!values.password) {
     errors.password = 'Password is required'
-  }else if( values.password.length < 5 ) {
+  } else if (values.password.length < 5) {
     errors.password = 'Please enter at least 5 characters password.'
   }
-  if ( values.password !== values.repeatPassword ) {
+  if (values.password !== values.repeatPassword) {
     errors.repeatPassword = 'Please match the repeat password.'
   }
   if (!values.accountAlias) {
@@ -95,10 +177,15 @@ export default connect(
     lang: state.core.lang
   }),
   (dispatch) => ({
-    registerKey: (token) => dispatch(actions.core.registerKey(token))
+    registerKey: (token) => dispatch(actions.core.registerKey(token)),
+    showError: (err) => dispatch({type: 'ERROR', payload: err}),
+    success: () => {
+      dispatch(push({pathname: '/transactions'}))
+      window.location.reload()
+    },
   })
 )(reduxForm({
   form: 'initDefaultPassword',
-  fields: ['keyAlias', 'password', 'repeatPassword', 'accountAlias' ],
+  fields: ['keyAlias', 'password', 'repeatPassword', 'accountAlias'],
   validate
 })(Register))
