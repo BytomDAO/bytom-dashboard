ec331c121d48cb877236ce67a5b5cf7891db2883
Yongfeng LI <wliyongfeng@gmail.com>
2018-04-20 18:09:28 +0800

    remove import from file to create key

--- src/features/mockhsm/components/New.jsx
+++ src/features/mockhsm/components/New.jsx
@@ -22,28 +22,6 @@ class New extends React.Component {
     })
   }
 
-  handleCheckedChange(e) {
-    this.setState({
-      checked: e.target.checked
-    })
-  }
-
-  handleFileChange(event) {
-    const files = event.target.files
-    if (files.length <= 0) {
-      this.setState({key: null})
-      return
-    }
-
-    const fileReader = new FileReader()
-    fileReader.onload = fileLoadedEvent => {
-      this.setState({
-        key: fileLoadedEvent.target.result
-      })
-    }
-    fileReader.readAsText(files[0], 'UTF-8')
-  }
-
   render() {
     const {
       fields: { alias, password, confirmPassword, accountAlias },
@@ -63,25 +41,6 @@ class New extends React.Component {
 
         <FormSection title={ lang === 'zh' ? '密钥信息' : 'Key Information' }>
           <TextField title={ lang === 'zh' ? '别名' : 'Alias'} placeholder={ lang === 'zh' ? '请输入密钥别名...' :'Please enter key alias...'} fieldProps={alias} autoFocus={true} />
-          <div>
-            <input type='checkbox' id='private_key_file_input'
-                   checked={this.state.import}
-                   onChange={this.handleCheckedChange.bind(this)}/>
-            <label htmlFor='private_key_file_input'
-                   style={{'marginLeft': '5px', 'userSelect': 'none'}}>
-              { lang === 'zh' ? '从文件中导入' : 'import from file' }
-            </label>
-          </div>
-          {
-            this.state.checked &&
-              <TextField title={ lang === 'zh' ? '账户别名' : 'Account alias'} placeholder={ lang === 'zh' ? '请输入账户别名...' : 'Please enter account alias...'} fieldProps={accountAlias}></TextField>
-          }
-          {
-            this.state.checked &&
-            <input type='file' style={{'display': 'flex', 'alignItems': 'center', 'fontSize': '12px'}}
-                   onChange={this.handleFileChange.bind(this)}/>
-          }
-
           <TextField title={ lang === 'zh' ? '密码' : 'Password'}  placeholder={ lang === 'zh' ? '请输入密码...' : 'Please enter your password...'} fieldProps={password} autoFocus={false} type={'password'} />
           <TextField title={ lang === 'zh' ? '重复密码' : 'Repeat Password'} placeholder={ lang === 'zh' ? '请重复输入密码...' : 'Please repeat your password'} fieldProps={confirmPassword} autoFocus={false} type={'password'} />
         </FormSection>
