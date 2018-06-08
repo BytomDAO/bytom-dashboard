b5ef782f3ec35d7320d43987cf867d0f8cfb4069
Zhiting Lin <zlin035@uottawa.ca>
2018-06-08 18:34:39 +0800

    add the syncing status

--- src/features/app/components/Sync/Sync.jsx
+++ src/features/app/components/Sync/Sync.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { ProgressBar, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import navStyles from '../Navigation/Navigation.scss'
 import styles from './Sync.scss'
 
@@ -16,21 +17,37 @@ class Sync extends React.Component {
     const highestBlock = coreData.highestBlock
     const lang = this.props.lang
 
+    const now = (currentBlock * 100/highestBlock).toFixed(1)
+    const tooltip = (
+      <Tooltip id='tooltip'>
+        {currentBlock}/{highestBlock} <strong>({now}%)</strong>
+      </Tooltip>
+    )
+    const progressInstance = (<ProgressBar className={styles.progressBar} now={now} />)
+
     if (syncing) {
       return <ul className={`${navStyles.navigation} ${styles.main}`}>
         <li key='sync-title' className={navStyles.navigationTitle}>{ networkID } { lang === 'zh' ? '同步状态' : 'Sync Status'}</li>
-        <li key='sync-status'>{lang === 'zh' ? '同步中: ' : 'Synchronizing: '}  {currentBlock}/{highestBlock}</li>
-        <li key='sync-peer-count'>{lang === 'zh' ? '连接数' : 'Peer Count'}: {peerCount}</li>
+        <li key='sync-peer-count' className={(peerCount>0)?styles.blockHightlight: null}>{lang === 'zh' ? '连接数' : 'Peer Count'}: {peerCount}</li>
+        <li key='sync-status'> <OverlayTrigger placement='top' overlay={tooltip}>
+          <div> {lang === 'zh' ? '区块同步中...' : 'Synchronizing...'} {progressInstance} </div>
+        </OverlayTrigger></li>
       </ul>
     }
 
     const elems = []
 
-    // elems.push(<li key='sync-networkID' className={navStyles.navigationTitle}>{ networkID }</li>)
     elems.push(<li key='sync-title' className={navStyles.navigationTitle}>{ networkID } { lang === 'zh' ? '同步状态' : 'Sync Status' }</li>)
+    elems.push(<li key='sync-peer-count' className={(peerCount>0)?styles.blockHightlight: null}>{lang === 'zh' ? '连接数' : 'Peer Count'}: {peerCount}</li>)
 
     if(!syncing && currentBlock == highestBlock){
-      elems.push(<li className={styles.blockHightlight} key='sync-done'>{lang === 'zh' ? '同步完成: ' : 'Fully synced: ' } <span>{currentBlock}/{highestBlock}</span></li>)
+      elems.push(<li className={styles.blockHightlight} key='sync-done'>
+        <OverlayTrigger placement='top' overlay={tooltip}>
+          <div>
+            {lang === 'zh' ? '同步完成 ' : 'Fully synced ' }{progressInstance}
+          </div>
+        </OverlayTrigger>
+      </li>)
     }
 
     if(!syncing && currentBlock < highestBlock){
