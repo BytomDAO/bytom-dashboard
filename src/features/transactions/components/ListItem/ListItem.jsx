b2ef19855e251fff5b6d278390db94de645134ac
Zhiting Lin <zlin035@uottawa.ca>
2018-09-11 19:16:54 +0800

    restyled the transaction detail items.

--- src/features/transactions/components/ListItem/ListItem.jsx
+++ src/features/transactions/components/ListItem/ListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
-import { Summary } from 'features/transactions/components'
+import { DetailSummary } from 'features/transactions/components'
 import { RelativeTime } from 'features/shared/components'
 import styles from './ListItem.scss'
 
@@ -11,33 +11,42 @@ class ListItem extends React.Component {
     const confirmation = item.highest - item.blockHeight + 1
     item.confirmations = confirmation
 
+    const immatured = confirmation> 0 && confirmation<100
+
     const unconfirmedTx = item.blockHeight === 0 && item.blockHash === '0000000000000000000000000000000000000000000000000000000000000000'
+
+
+    const confirmView =(<span>
+          <span className={immatured? 'text-danger': null}>{confirmation}</span>
+      {lang === 'zh' ? ` 确认数${immatured?' (100)': ''}` :' of 100 confirmations'}
+      </span>)
 
     return(
       <div className={styles.main}>
         <div className={styles.titleBar}>
           <div className={styles.title}>
-            <label>{lang === 'zh' ? '交易ID:' : 'Transaction ID:'}</label>
+            <label>{lang === 'zh' ? '交易ID:' : 'Tx ID:'}</label>
             &nbsp;<code>{item.id}</code>&nbsp;
-            {unconfirmedTx? null : <span className={styles.timestamp}>
-              <RelativeTime timestamp={item.timestamp} />
-            </span>}
 
-            <span className={`${styles.timestamp} ${unconfirmedTx?'text-danger': null}`}>
+            <span className={`${styles.confirmation} ${unconfirmedTx?'text-danger': null}`}>
               {
                 unconfirmedTx ?
                   (lang === 'zh' ? '未确认交易' : 'unconfirmed Transaction'):
-                  ((confirmation)+(lang === 'zh' ? ' 确认数' :` confirmation${confirmation > 1 ? 's' : ''}`))
+                  confirmView
               }
             </span>
 
           </div>
+          {unconfirmedTx? null : <span className={styles.timestamp}>
+              <RelativeTime timestamp={item.timestamp} />
+            </span>}
           <Link className={styles.viewLink} to={`/transactions/${item.id}`}>
             {lang === 'zh' ? '查看详情' : 'View details'}
           </Link>
         </div>
 
-        <Summary transaction={item} lang={lang} btmAmountUnit={this.props.btmAmountUnit}/>
+        <DetailSummary transaction={item} lang={lang} btmAmountUnit={this.props.btmAmountUnit}/>
+
       </div>
     )
   }
