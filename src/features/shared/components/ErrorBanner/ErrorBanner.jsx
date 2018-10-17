e05952cc6d5e909df7d54b0db07b84211624d6cd
Zhiting Lin <zlin035@uottawa.ca>
2018-10-17 10:18:30 +0800

    add the error message translation.

--- src/features/shared/components/ErrorBanner/ErrorBanner.jsx
+++ src/features/shared/components/ErrorBanner/ErrorBanner.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './ErrorBanner.scss'
+import {withNamespaces} from 'react-i18next'
 
 class ErrorBanner extends React.Component {
   render() {
+    const {t, i18n} = this.props
     const error = this.props.error || ''
+    const codeMessage = error.code && i18n.exists(`btmError.${error.code}`) && t(`btmError.${error.code}`)
     const success = this.props.success
-    const message = error.chainMessage || error.message || error || success
+    const message = codeMessage || error.chainMessage || error.message || error || success
 
     return (
       <div className={success? styles.mainSuccess:  styles.main }>
@@ -13,17 +16,17 @@ class ErrorBanner extends React.Component {
 
         {message &&
           <div className={(error.code || error.requestId) ? styles.message : ''}>
-            {message}{error.detail ? `: ${error.detail}` : ''}
+            {message}
           </div>}
 
         {error.code &&
-          <div className={styles.extra}>Error Code: <strong>{error.code}</strong></div>}
+          <div className={styles.extra}>{t('commonWords.errorCode')} <strong>{error.code}</strong></div>}
 
         {error.requestId &&
-          <div className={styles.extra}>Request ID: <strong>{error.requestId}</strong></div>}
+          <div className={styles.extra}>{t('commonWords.requestId')} <strong>{error.requestId}</strong></div>}
       </div>
     )
   }
 }
 
-export default ErrorBanner
+export default withNamespaces('translations')  (ErrorBanner)
