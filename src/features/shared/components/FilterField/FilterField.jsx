3347f5d4a9c51f44788946cdd16002ff73718c99
Zhiting Lin <zlin035@uottawa.ca>
2019-12-19 11:13:18 +0800

    update search box to dropdown

--- src/features/shared/components/FilterField/FilterField.jsx
+++ src/features/shared/components/FilterField/FilterField.jsx
@@ -1,43 +1,76 @@
 import React from 'react';
-import { Autocomplete } from '../';
 import styles from './FilterField.scss'
+import { DropdownButton, MenuItem } from 'react-bootstrap'
+import {connect} from 'react-redux'
+import actions from 'actions'
 
 class FilterField extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props)
 
     this.state = {
-      value: '',
-    };
-    this.onChange = this.onChange.bind(this)
+      showDropdown: false,
+      selected: props.selected || '全部'
+    }
+
+    this.select = this.select.bind(this)
+    this.toggleDropwdown = this.toggleDropwdown.bind(this)
+    this.closeDropdown = this.closeDropdown.bind(this)
   }
 
-  onChange( newValue) {
-    this.setState({
-      value: newValue
-    });
-  };
+  componentDidMount() {
+    if (!this.props.autocompleteIsLoaded) {
+      this.props.fetchAll().then(() => {
+        this.props.didLoadAutocomplete()
+      })
+    }
+  }
 
+  toggleDropwdown() {
+    this.setState({ showDropdown: !this.state.showDropdown })
+  }
+
+  closeDropdown() {
+    this.setState({ showDropdown: false })
+  }
+
+  select(id) {
+
+    const alias = id == 'all'? '全部' : (this.props.items.filter((item) => item.id === id))[0].alias
+    this.setState({ selected: alias })
+    this.props.pushList({asset_id: id})
+    this.closeDropdown()
+  }
 
   render() {
-    const { value } = this.state;
-    const inputProps = {
-      value,
-      onChange: this.onChange
-    };
     return (
       <div className={styles.container}>
         <div>存证类型：</div>
-        <Autocomplete.AssetAlias
-          fieldProps={inputProps}
-          shouldRenderSuggestions={()=>true}
-          onSuggestionSelected={(e)=>{
-            e.preventDefault()
-            this.props.pushList({asset_alias: this.state.value})}}
-        />
+        <DropdownButton
+          title={this.state.selected}
+          onSelect={this.select}
+        >
+          <MenuItem eventKey={'all'}>全部</MenuItem>
+          {this.props.items.map((option) =>
+            <MenuItem eventKey={option.id}>{option.alias}</MenuItem>
+          )}
+        </DropdownButton>
       </div>
     )
   }
 }
 
-export default FilterField
+export const mapStateToProps = (type) => (state) => ({
+  autocompleteIsLoaded: state[type].autocompleteIsLoaded,
+  items: Object.keys(state[type].items).map(k => state[type].items[k])
+})
+
+export const mapDispatchToProps = (type) => (dispatch) => ({
+  didLoadAutocomplete: () => dispatch(actions[type].didLoadAutocomplete),
+  fetchAll: () => dispatch(actions[type].fetchAll())
+})
+
+export default connect(
+  mapStateToProps('asset'),
+  mapDispatchToProps('asset')
+)(FilterField)
