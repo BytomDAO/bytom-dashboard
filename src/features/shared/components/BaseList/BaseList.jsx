b058557016fb731065c6d0b3261b8fd4135a61d1
Zhiting Lin <zlin035@uottawa.ca>
2019-12-19 10:19:53 +0800

    Revert "update tripple tags"
    This reverts commit dfa21b5a76cc6e274ec5b9edd5e0995327c4d04c.

--- src/features/shared/components/BaseList/BaseList.jsx
+++ src/features/shared/components/BaseList/BaseList.jsx
@@ -17,9 +17,9 @@ class ItemList extends React.Component {
     const t = this.props.t
 
     const objectName = humanize(t(`crumbName.${type.toLowerCase()}` ))
-    const title = this.props.title || capitalize(t(`crumbName.${type.toLowerCase()}` ) || label)
+    const title = capitalize(t(`crumbName.${type.toLowerCase()}` ) || label)
 
-    const newButton = <button key='showCreate' className='btn btn-primary' onClick={() =>this.props.showCreate(this.props.title)}>
+    const newButton = <button key='showCreate' className='btn btn-primary' onClick={this.props.showCreate}>
       + { this.props.createLabel || t('crumbName.new')}
     </button>
     if (!this.props.skipCreate) {
@@ -64,7 +64,6 @@ class ItemList extends React.Component {
         <this.props.listItemComponent
           key={item.id}
           item={item}
-          title={this.props.title}
           btmAmountUnit={this.props.btmAmountUnit}
           {...this.props.itemActions}/>)
 
@@ -137,7 +136,7 @@ export const mapStateToProps = (type, itemComponent, additionalProps = {}) => (s
 export const mapDispatchToProps = (type) => (dispatch) => {
   return {
     pushList: (query, pageNumber) => dispatch(actions[type].pushList(query, pageNumber)),
-    showCreate: (title) => dispatch(actions[type].showCreate(title)),
+    showCreate: () => dispatch(actions[type].showCreate),
   }
 }
 
